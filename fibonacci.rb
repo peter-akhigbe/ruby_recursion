45cbe48b14c4872688c98249365de28879c0891b
Peter Akhigbe <akhigbe37@gmail.com>
2023-10-10 10:15:46 +0100

    Update fibonacci.rb

--- fibonacci.rb
+++ fibonacci.rb
@@ -14,11 +14,11 @@ def fibs_rec(num, array = [0, 1])
     return [0] if num == 1
     return array if num == 2
 
-    next_fib = array[-1] + array[-2]
-    fibs_rec(num - 1, array << next_fib)
+    array << array[-1] + array[-2]
+    fibs_rec(num - 1, array)
 end
 
-num = 100
+num = 10
 
 p fibs(num)
 p fibs_rec(num)
